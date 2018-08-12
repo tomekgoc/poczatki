70d8e1dc1fc287c308b3e240971d55a4f5d37bd0
tomekgoc <42316165+tomekgoc@users.noreply.github.com>
2018-08-12 16:46:01 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -26,18 +26,24 @@ public class Main {
     public static void addTask(String task) {
 	    boolean hasCapacityForNewTask = tasksCount < tasks.length;
 	    if (hasCapacityForNewTask) {
-	        tasks[tasksCount] = task;
-	        tasksCount++;
+			tasks[tasksCount] = task;
+			tasksCount++;
+			addLog("Dodano nowe zadanie: " + task);
+		} else {
+	    	addLog("Nie udało się dodać nowego zadania: " + task);
         }
     }
 
     public static void removeTask(int indexTask) {
 	    boolean isTaskExist = indexTask < tasksCount;
 	    if (isTaskExist) {
-	        for (int i=indexTask; i<tasksCount-1; i++) {
-	            tasks[i] = tasks[i+1];
-            }
-            tasksCount--;
+			addLog("Usunięto zadanie: " + tasks[indexTask]);
+			for (int i = indexTask; i < tasksCount - 1; i++) {
+				tasks[i] = tasks[i + 1];
+			}
+			tasksCount--;
+		} else {
+	    	addLog("Nie udało się usunąć zadania: " + tasks[indexTask]);
         }
     }
 
@@ -46,7 +52,56 @@ public class Main {
 	    for (int i=0; i<tasksCount; i++) {
 	        System.out.println(tasks[i]);
         }
+        addLog("Wyświetlono listę zadań");
     }
+
+    public static void addProject(String project) {
+		boolean hasCapacityForNewProject = projectsCount < projects.length;
+		if (hasCapacityForNewProject) {
+			projects[projectsCount] = project;
+			projectsCount++;
+			addLog("Dodano nowy projekt: " + project);
+		} else {
+			addLog("Nie udało się dodać nowego projektu: " + project);
+		}
+	}
+
+	public static void removeProject(int indexProject) {
+		boolean isProjectExist = indexProject < projectsCount;
+		if (isProjectExist) {
+			addLog("Usunięto projekt: " + projects[indexProject]);
+			for (int i = indexProject; i < projectsCount - 1; i++) {
+				projects[i] = projects[i + 1];
+			}
+			projectsCount--;
+		} else {
+			addLog("Nie udało się usunąć projektu: " + projects[indexProject]);
+		}
+	}
+
+	public static void displayProject() {
+		System.out.println("List of projects: ");
+		for (int i=0;i<projectsCount;i++) {
+			System.out.println(projects[i]);
+		}
+		addLog("Wyświetlono listę projektów");
+	}
+
+	public static void addLog(String log) {
+		boolean hasCapacityForNewLog = changeLogCount < changeLog.length;
+		if (hasCapacityForNewLog) {
+			changeLog[changeLogCount] = log;
+			changeLogCount++;
+		}
+	}
+
+	public static void displayChangeLog() {
+		System.out.println("Change log: ");
+		for (int i=0;i<changeLogCount;i++) {
+			System.out.println(changeLog[i]);
+		}
+	}
+
 	public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
 	    int radix;
@@ -67,9 +122,25 @@ public class Main {
                     int index = scanner.nextInt();
                     removeTask(index-1);
                     break;
-                case 5:
+				case 3:
+					System.out.println("Podaj nazwe projektu do dodania: ");
+					String project = scanner.next();
+					addProject(project);
+					break;
+				case 4:
+					System.out.println("Podaj indeks projektu do usunięcia:");
+					int indexProject = scanner.nextInt();
+					removeProject(indexProject);
+					break;
+				case 5:
                     displayTasks();
                     break;
+				case 6:
+					displayProject();
+					break;
+				case 7:
+					displayChangeLog();
+					break;
                 case 0:
                     isApplicationRun=false;
                     break;
